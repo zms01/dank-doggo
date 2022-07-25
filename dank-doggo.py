abde9a37a6f7685357397d795b7277a6a2c49a04
zms01 <mikezhelezov@gmail.com>
2022-07-25 13:24:48 +0300

    Update dank-doggo.py

--- dank-doggo.py
+++ dank-doggo.py
@@ -35,7 +35,7 @@ if credentials["channel"] == "":
 
 token = credentials["token"]
 channel = credentials["channel"]
-sub = "dogpictures"
+sub = credentials["subreddit"]
 start_time = datetime.utcnow().timestamp()
 
 def prev_submissions():
